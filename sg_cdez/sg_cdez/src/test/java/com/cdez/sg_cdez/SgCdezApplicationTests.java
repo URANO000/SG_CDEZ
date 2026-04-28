205ec1275c76e2babd78686c34171fa32d9d7fd4
URANO000 <kkonigin1100@gmail.com>
2026-04-28 01:07:00 -0600

    Springboot + Java

---
+++ sg_cdez/sg_cdez/src/test/java/com/cdez/sg_cdez/SgCdezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdez.sg_cdez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgCdezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
