26624156da2e692e2414d9131e450ae7a9929c75
URANO000 <kkonigin1100@gmail.com>
2026-05-27 01:03:45 -0600

    Added necessary resources for personal API

---
+++ sg_cdez_api/src/main/java/com/cdez/sg_cdez_api/controller/PersonalController.java
@@ -0,0 +1,10 @@
+package com.cdez.sg_cdez_api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/personal")
+@RequiredArgsConstructor
+public class PersonalController {
+}
---
+++ sg_cdez_api/src/main/java/com/cdez/sg_cdez_api/service/impl/PersonalServiceImpl.java
@@ -0,0 +1,6 @@
+package com.cdez.sg_cdez_api.service.impl;
+
+import com.cdez.sg_cdez_api.service.PersonalService;
+
+public class PersonalServiceImpl implements PersonalService {
+}
