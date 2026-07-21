73ccafd154885e73f6ada88ebb5188e7bf98d71a
URANO000 <kkonigin1100@gmail.com>
2026-07-21 13:34:41 -0600

    Update PersonalRepository.java

--- sg_cdez_api/src/main/java/com/cdez/sg_cdez_api/repository/PersonalRepository.java
+++ sg_cdez_api/src/main/java/com/cdez/sg_cdez_api/repository/PersonalRepository.java
@@ -1,11 +1,19 @@
 package com.cdez.sg_cdez_api.repository;
 
 import com.cdez.sg_cdez_api.entity.Personal;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 import java.util.UUID;
 
 public interface PersonalRepository extends JpaRepository<Personal, UUID> {
     boolean existsByUsuario(String usuario);
     Optional<Personal> findByUsuario(String usuario);
+    Page<Personal> findByEspecialidad(String especialidad, Pageable pageable);
+    Page<Personal> findByActivo(boolean activo, Pageable pageable);
+
+    @Query("SELECT ")
+    Page<Personal> findByNombreCompleto(String nombreCompleto);
 }
