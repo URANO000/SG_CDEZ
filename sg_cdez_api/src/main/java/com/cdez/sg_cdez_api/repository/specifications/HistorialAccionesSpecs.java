001dba2ae95d0f03334d39e385942b370d1d8ad2
URANO000 <kkonigin1100@gmail.com>
2026-07-26 15:48:03 -0600

    Specifications

--- sg_cdez_api/src/main/java/com/cdez/sg_cdez_api/repository/specifications/HistorialAccionesSpecs.java
+++ sg_cdez_api/src/main/java/com/cdez/sg_cdez_api/repository/specifications/HistorialAccionesSpecs.java
@@ -1,4 +1,71 @@
 package com.cdez.sg_cdez_api.repository.specifications;
 
+import com.cdez.sg_cdez_api.entity.Consulta;
+import com.cdez.sg_cdez_api.entity.HistorialAcciones;
+import jakarta.persistence.criteria.Expression;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class HistorialAccionesSpecs {
+    public static Specification<HistorialAcciones> hasDate(LocalDate providedDate){
+        return (root, query, criteriaBuilder) -> {
+            LocalDateTime startOfDay = providedDate.atStartOfDay();
+            LocalDateTime endOfDay = providedDate.plusDays(1).atStartOfDay();
+
+            return criteriaBuilder.and(
+                    criteriaBuilder.greaterThanOrEqualTo(root.get("performedAt"), startOfDay),
+                    criteriaBuilder.lessThan(root.get("performedAt"), endOfDay)
+            );
+        };
+    }
+
+    public static Specification<Consulta> hasDateRange(LocalDate providedStartDate, LocalDate providedEndDate){
+        return (root, query, criteriaBuilder) -> {
+            LocalDateTime startDate= providedStartDate.atStartOfDay();
+            LocalDateTime endDate = providedEndDate.plusDays(1).atStartOfDay();
+
+            return criteriaBuilder.and(
+                    criteriaBuilder.greaterThanOrEqualTo(root.get("performedAt"), startDate),
+                    criteriaBuilder.lessThan(root.get("performedAt"), endDate)
+            );
+        };
+    }
+
+    public static Specification<HistorialAcciones> containsSearch(String providedSearch){
+        return (root, query, criteriaBuilder) -> {
+            String pattern = "%" + providedSearch.toLowerCase().trim() + "%";
+
+            Expression<String> fullName =
+                    criteriaBuilder.concat(
+                            criteriaBuilder.concat(
+                                    criteriaBuilder.concat(
+                                            criteriaBuilder.coalesce(root.get("performedBy").get("primerNombre"), ""),
+                                            " "
+                                    ),
+                                    criteriaBuilder.concat(
+                                            criteriaBuilder.coalesce(root.get("performedBy").get("segundoNombre"), ""),
+                                            " "
+                                    )
+                            ),
+                            criteriaBuilder.concat(
+                                    criteriaBuilder.concat(
+                                            criteriaBuilder.coalesce(root.get("performedBy").get("primerApellido"), ""),
+                                            criteriaBuilder.literal(" ")
+                                    ),
+                                    criteriaBuilder.coalesce(root.get("performedAt").get("segundoApellido"), "")
+                            )
+                    );
+            return criteriaBuilder.or(
+                    criteriaBuilder.like(criteriaBuilder.lower(root.get("performedBy").get("primerNombre")), pattern),
+                    criteriaBuilder.like(criteriaBuilder.lower(root.get("performedBy").get("segundoNombre")), pattern),
+                    criteriaBuilder.like(criteriaBuilder.lower(root.get("performedBy").get("primerApellido")), pattern),
+                    criteriaBuilder.like(criteriaBuilder.lower(root.get("performedBy").get("segundoApellido")), pattern),
+                    criteriaBuilder.like(criteriaBuilder.lower(fullName), pattern)
+            );
+
+
+        };
+    }
 }
--- sg_cdez_api/src/main/java/com/cdez/sg_cdez_api/service/HistorialAccionesService.java
+++ sg_cdez_api/src/main/java/com/cdez/sg_cdez_api/service/HistorialAccionesService.java
@@ -1,7 +1,16 @@
 package com.cdez.sg_cdez_api.service;
 
+import com.cdez.sg_cdez_api.dto.request.HistorialAccionesCreateRequest;
+import com.cdez.sg_cdez_api.dto.request.HistorialAccionesFiltro;
+import com.cdez.sg_cdez_api.dto.response.HistorialAccionesResponse;
+import com.cdez.sg_cdez_api.dto.response.PageResponse;
 import org.springframework.stereotype.Service;
+
+import java.awt.print.Pageable;
 
 @Service
 public interface HistorialAccionesService {
+    PageResponse<HistorialAccionesResponse> listarHistorialFiltrado(HistorialAccionesFiltro filtros, Pageable pageable);
+    HistorialAccionesResponse obtenerHistorialPorId(int id);
+    HistorialAccionesResponse crearHistorial(HistorialAccionesCreateRequest request);
 }
