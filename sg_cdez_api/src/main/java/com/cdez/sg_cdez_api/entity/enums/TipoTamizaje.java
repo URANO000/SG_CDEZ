ce57fc3c8722f6cebfe77dc4081dbea5eebb6bc4
URANO000 <kkonigin1100@gmail.com>
2026-08-30 23:02:21 -0600

    Update TipoTamizaje.java

--- sg_cdez_api/src/main/java/com/cdez/sg_cdez_api/entity/enums/TipoTamizaje.java
+++ sg_cdez_api/src/main/java/com/cdez/sg_cdez_api/entity/enums/TipoTamizaje.java
@@ -1,8 +1,30 @@
 package com.cdez.sg_cdez_api.entity.enums;
 
+import lombok.Getter;
+
 public enum TipoTamizaje {
-    MNA,
-    SARC_F,
-    MUST,
-    NRS
+    MNA(Especialidad.NUTRICION, true),
+    SARC_F(Especialidad.NUTRICION, true),
+    MUST(Especialidad.NUTRICION, true),
+    NRS(Especialidad.NUTRICION, true),
+
+    COGNITIVA(Especialidad.PSICOLOGIA, false),
+    ANSIEDAD(Especialidad.PSICOLOGIA, false),
+    OTROS(Especialidad.PSICOLOGIA, false);
+
+    @Getter
+    private final Especialidad especialidad;
+    private final boolean requierePuntaje;
+
+    TipoTamizaje(
+            Especialidad especialidad,
+            boolean requierePuntaje
+    ) {
+        this.especialidad = especialidad;
+        this.requierePuntaje = requierePuntaje;
+    }
+
+    public boolean requierePuntaje() {
+        return requierePuntaje;
+    }
 }
